498413da9e1a59eb6ae375982f63f5a32a820564
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-05-26 15:55:01 -0700

    sanity check on output file

--- batch/templates/fill_trees.sh
+++ batch/templates/fill_trees.sh
@@ -12,9 +12,14 @@ echo "Starting on `hostname`, `date`"
 mkdir -p ${SCRATCH_DIR}
 cd ${SCRATCH_DIR}
 ${BASE_DIR}/python/count_tree_entries.py -i ${BASE_DIR}/${IN_FILE} -o ${OUT_FILE}
-echo "copying output file ${OUT_FILE}"
-ls -lh ${OUT_FILE}
-cp -p ${OUT_FILE} ${DEST_DIR}
+if [[ -e ${OUT_FILE} ]]
+then
+    echo "copying output file ${OUT_FILE}"
+    ls -lh ${OUT_FILE}
+    cp -p ${OUT_FILE} ${DEST_DIR}
+else
+    echo "cannot find output file ${OUT_FILE}"
+fi
 cd ${BASE_DIR}
 echo "cleaning up ${SCRATCH_DIR}"
 rm -rf ${SCRATCH_DIR}
