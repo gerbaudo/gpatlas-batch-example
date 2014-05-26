d3bab47247f3eee8185f234eb7e4186543e8ae1c
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-05-26 15:55:50 -0700

    bugfix usage option

--- python/count_tree_entries.py
+++ python/count_tree_entries.py
@@ -7,7 +7,7 @@ r.gROOT.SetBatch(True)                     # no windows popping up
 r.PyConfig.IgnoreCommandLineOptions = True # don't let root steal our cmd-line options
 
 def main():
-    parser = optparse.OptionParser(usage=usage)
+    parser = optparse.OptionParser()
     parser.add_option("-i", "--input", help="list of root files")
     parser.add_option("-o", "--output", help="output root file")
     (options, args) = parser.parse_args()
